73c555413ec38e13424e9b83e506bfbc59c83829
Bailey <bailey.tan.nguyen@gmail.com>
2024-03-27 20:08:59 -0500

    Initial commit

---
+++ src/main/java/edu/uta/loadtestingproject/LoadTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.uta.loadtestingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadTestingProjectApplication.class, args);
+    }
+
+}
